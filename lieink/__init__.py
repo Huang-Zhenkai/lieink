37e26e99406a85a9efcf2f2f771ca35b26affb31
Zhenkai Huang <1984207929@qq.com>
2026-09-09 13:08:54 +0800

    发现了一个导入错误

--- lieink/__init__.py
+++ lieink/__init__.py
@@ -3,5 +3,6 @@ from .annotations import (
     get_NDArrayAnnotation,  # type: ignore
     get_NDArrayAnnotationShape,  # type: ignore
 )
-from .basic_containers import BasicContainer, BasicLie  # type: ignore
+from .basic_atoms import BasicLie  # type: ignore
+from .basic_containers import BasicContainer  # type: ignore
 from .utils import check_equal, get_Type, get_TypeName, print4  # type: ignore
